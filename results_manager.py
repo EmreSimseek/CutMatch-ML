d6f97831dd81380fb5a55fcc9fc294b803135c61
EmreSimseek <emre0816s@gmail.com>
2024-10-07 22:21:58 +0300

    Update results_manager.py

--- results_manager.py
+++ results_manager.py
@@ -3,7 +3,10 @@ class ResultsManager:
     def __init__(self):
         self.results = []
 
-    def add_result(self, file_name, mean_distance, std_distance, shaper_result, angle_analysis, same_series_value):
+    def add_result(self, file_name, mean_distance, std_distance, shaper_result, angle_analysis,fourier_result, same_series_value):
+        if mean_distance is None or std_distance is None or not shaper_result or not angle_analysis or not fourier_result:
+            print(f"Sonuçlar eksik: {file_name}")
+            return  # Hatalı sonuç eklememek için geri dön
         """Sonuçları listeye ekler"""
         self.results.append({
             "file_name": file_name,
@@ -17,6 +20,12 @@ class ResultsManager:
             "angle_std_curr": f"{angle_analysis['std_curr']:.3f}",
             "angle_mse": f"{angle_analysis['mse']:.3f}",
             "similarity_score": f"{angle_analysis['similarity_score']:.3f}",
+            "min_freq": f"{fourier_result['min_freq']:.3f}",
+            "max_freq": f"{fourier_result['max_freq']:.3f}",
+            "mean_magnitude_prev": f"{fourier_result['mean_magnitude_prev']:.3f}",
+            "max_magnitude_prev": f"{fourier_result['max_magnitude_prev']:.3f}",
+            "mean_magnitude_curr": f"{fourier_result['mean_magnitude_curr']:.3f}",
+            "max_magnitude_curr": f"{fourier_result['max_magnitude_curr']:.3f}",
             "same_series_value": same_series_value
         })
 
